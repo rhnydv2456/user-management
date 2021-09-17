1f3396ecdf415fb8497653bb2b2d02c085bad086
beachesoverbtches <rohany245@gmail.com>
2021-09-18 00:10:52 +0530

    redirect to table page

--- functions/routes/api/users.js
+++ functions/routes/api/users.js
@@ -64,7 +64,7 @@ router.post('/', auth, [
 
         await user.save();
 
-        res.send('Data saved successfully!');
+        res.json({ success: 'Data saved successfully!' });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
